fb1f1e33b187d8b4e4a7d5db86e7a21e44cad8c4
shimr <rory.shim@gmail.com>
2016-07-27 16:16:12 -0400

    Create init.pp

---
+++ site/nginx/manifests/init.pp
@@ -0,0 +1,36 @@
+class nginx {
+
+  package { 'nginx':
+  ensure => present,
+  }
+  
+  file { '/var/www':
+  ensure => directory,
+  }
+  
+  file { '/etc/nginx/nginx.conf':
+  ensure => file,
+#  require => Package['nginx'],
+  source => 'puppet:///modules/nginx/default.conf',
+  }
+  
+  source => 'puppet:///modules/nginx/nginx.conf',
+  }
+  
+  file { '/etc/nginx/nginx.d/default.conf':
+  ensure => file,
+#  require => Package['nginx'],
+  source => 'puppet:///modules/nginx/default.conf',
+  }
+  
+  file { '/var/www/index.html':
+  ensure => file,
+  source => 'puppet:///modules/nginx/index.html,
+  }
+  
+  service { 'nginx':
+  ensure => running,
+  enable => true,
+  require => File['/etc/nginx/nginx.conf'],
+  }
+}
