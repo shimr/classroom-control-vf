509bc73ddfed0d45ee4d0e88581b8f09f550e4b9
shimr <rory.shim@gmail.com>
2016-07-28 12:29:35 -0400

    Create managed_user.pp

---
+++ site/users/manifests/managed_user.pp
@@ -0,0 +1,18 @@
+define users::managed_user (
+  $group = $title,
+  ) {
+  user { $title:
+    ensure => present,
+  }
+  file { "/home/${title}":
+    ensure => directory,
+    owner  => $title,
+    group  => $group,
+  }
+  file { "/home/${title}/.ssh":
+    ensure => directory,
+    owner  => $title,
+    group  => $group,
+    mode   => '0600',
+  }
+}  
