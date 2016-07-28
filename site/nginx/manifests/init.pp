cf33209abf432edd3f469f75febef9eeffb7c258
shimr <rory.shim@gmail.com>
2016-07-28 09:14:23 -0400

    Update init.pp

--- site/nginx/manifests/init.pp
+++ site/nginx/manifests/init.pp
@@ -1,33 +1,46 @@
 class nginx {
-
   package { 'nginx':
-  ensure => present,
+    ensure => present,
   }
-  
   file { '/var/www':
-  ensure => directory,
+    ensure => directory,
+    owner  => 'root',
+    group  => 'root',
+    mode   => '0775',
   }
-  
-  file { '/etc/nginx/nginx.conf':
-  ensure => file,
-#  require => Package['nginx'],
-  source => 'puppet:///modules/nginx/nginx.conf',
-  }
-  
-  file { '/etc/nginx/conf.d/default.conf':
-  ensure => file,
-#  require => Package['nginx'],
-  source => 'puppet:///modules/nginx/default.conf',
-  }
-  
   file { '/var/www/index.html':
-  ensure => file,
-  source => 'puppet:///modules/nginx/index.html',
+    ensure => file,
+    owner  => 'root',
+    group  => 'root',
+    mode   => '0664',
+    source => 'puppet:///modules/nginx/index.html',
   }
-  
+  file { '/etc/nginx/nginx.conf':
+    ensure  => file,
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0664',
+    source  => 'puppet:///modules/nginx/nginx.conf',
+    require => Package['nginx'],
+    notify  => Service['nginx'],
+  }
+  file { '/etc/nginx/conf.d':
+    ensure => directory,
+    owner  => 'root',
+    group  => 'root',
+    mode   => '0775',
+  }
+  file { '/etc/nginx/conf.d/default.conf':
+    ensure  => file,
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0664',
+    source  => 'puppet:///modules/nginx/default.conf',
+    require => Package['nginx'],
+    notify  => Service['nginx'],
+  }
   service { 'nginx':
-  ensure => running,
-  enable => true,
-  require => File['/etc/nginx/nginx.conf'],
+    ensure    => running,
+    enable    => true,
   }
 }
