4ec43a0bdbceaa75b31d83fd4caa08464ec38ccd
shimr <rory.shim@gmail.com>
2016-07-28 12:49:17 -0400

    Create admins.pp

---
+++ site/users/manifests/admins.pp
@@ -0,0 +1,12 @@
+class users::admins {
+users::managed_user { 'joe': }
+users::managed_user { 'alice':
+  group => 'staff',
+  }
+users::managed_user { 'chen':
+  group => 'staff',
+  }
+group { 'staff':
+  ensure => present,
+  }
+}
