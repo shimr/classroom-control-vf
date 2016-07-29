a511dca82159a396adeee8402a08ae759f44b648
shimr <rory.shim@gmail.com>
2016-07-29 11:58:31 -0400

    Create params.pp

---
+++ site/nginx/manifests/params.pp
@@ -0,0 +1,33 @@
+class nginx::params {
+    case $::osfamily {
+    'redhat' : {
+      $package = 'nginx'
+      $owner   = 'root'
+      $group   = 'root'
+      $default_docroot = '/var/www'
+      $confdir = '/etc/nginx'
+      $logdir  = '/var/log/nginx'
+      $user    = 'nginx'
+    }
+    'debian' : {
+      $package = 'nginx'
+      $owner   = 'root'
+      $group   = 'root'
+      $default_docroot = '/var/www'
+      $confdir = '/etc/nginx'
+      $logdir  = '/var/log/nginx'
+      $user    = 'www-data'
+    }
+    'windows' : {
+      $package = 'nginx-service'
+      $owner   = 'Administrator'
+      $group   = 'Administrators'
+      $default_docroot = 'C:/ProgramData/nginx/html'}
+      $confdir = 'C:/ProgramData/nginx'
+      $logdir  = 'C:/ProgramData/nginx/logs'
+      $user    = 'nobody'
+    }
+    default   : {
+      fail("Module ${module_name} is not supported on ${::osfamily}")
+    }
+}
