782449c7f189f5c5aafd9d05faf0e327c26f2ede
shimr <rory.shim@gmail.com>
2016-07-29 12:15:26 -0400

    Update init.pp

--- site/nginx/manifests/init.pp
+++ site/nginx/manifests/init.pp
@@ -1,38 +1,49 @@
 class nginx  (
   $root = undef,
-){
-
-  case $::osfamily {
-    'redhat' : {
-      $package = 'nginx'
-      $owner   = 'root'
-      $group   = 'root'
-      if $root != undef {$docroot = $root} else {$docroot = '/var/www'}
-      $confdir = '/etc/nginx'
-      $logdir  = '/var/log/nginx'
-      $user    = 'nginx'
-    }
-    'debian' : {
-      $package = 'nginx'
-      $owner   = 'root'
-      $group   = 'root'
-      if $root != undef {$docroot = $root} else {$docroot = '/var/www'}
-      $confdir = '/etc/nginx'
-      $logdir  = '/var/log/nginx'
-      $user    = 'www-data'
-    }
-    'windows' : {
-      $package = 'nginx-service'
-      $owner   = 'Administrator'
-      $group   = 'Administrators'
-      if $root != undef {$docroot = $root} else {$docroot = 'C:/ProgramData/nginx/html'}
-      $confdir = 'C:/ProgramData/nginx'
-      $logdir  = 'C:/ProgramData/nginx/logs'
-      $user    = 'nobody'
-    }
-    default   : {
-      fail("Module ${module_name} is not supported on ${::osfamily}")
-    }
+  $package = $nginx::params::package,
+  $owner   = $nginx::params::owner,
+  $group   = $nginx::params::group,
+  $default_docroot = $nginx::params::default_docroot,
+  $confdir = $nginx::params::confdir,
+  $logdir  = $nginx::params::logdir,
+  $user    = $nginx::params::user,
+) inherits nginx::params {
+#  case $::osfamily {
+#    'redhat' : {
+#      $package = 'nginx'
+#      $owner   = 'root'
+#      $group   = 'root'
+#      if $root != undef {$docroot = $root} else {$docroot = '/var/www'}
+#      $confdir = '/etc/nginx'
+#      $logdir  = '/var/log/nginx'
+#      $user    = 'nginx'
+#    }
+#    'debian' : {
+#      $package = 'nginx'
+#      $owner   = 'root'
+#      $group   = 'root'
+#      if $root != undef {$docroot = $root} else {$docroot = '/var/www'}
+#      $confdir = '/etc/nginx'
+#      $logdir  = '/var/log/nginx'
+#      $user    = 'www-data'
+#    }
+#    'windows' : {
+#      $package = 'nginx-service'
+#      $owner   = 'Administrator'
+#      $group   = 'Administrators'
+#      if $root != undef {$docroot = $root} else {$docroot = 'C:/ProgramData/nginx/html'}
+#      $confdir = 'C:/ProgramData/nginx'
+#      $logdir  = 'C:/ProgramData/nginx/logs'
+#      $user    = 'nobody'
+#    }
+#    default   : {
+#      fail("Module ${module_name} is not supported on ${::osfamily}")
+#    }
+#  }
+ 
+ $docroot = $root? {
+   undef => $default_docroot
+   default => $root
   }
   
   package { $package:
